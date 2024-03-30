4ff04372ed86488a5f43c67c0d38babe5e596b31
Marcos Brito <marcos.brito0507@gmail.com>
2024-03-30 19:28:13 -0300

    feat: implement create command

--- internal/commands.go
+++ internal/commands.go
@@ -1,10 +1,28 @@
 package internal
 
 import (
+	"os"
+	"strconv"
+
+	"github.com/marcos-brito/sozza/internal/connector"
+	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 )
 
 func Create(ctx *cli.Context) error {
+	db := connector.PickConnector(ctx.String("dbname")).Connect(ctx.String("url"))
+	schema, err := os.ReadFile(ctx.String("schema"))
+
+	if err != nil {
+		log.Fatalf("Could not read the schema file: %s", err)
+	}
+
+	_, err = db.Exec(string(schema))
+
+	if err != nil {
+		log.Fatalf("Something went wrong executing the schema: %s", err)
+	}
+
 	return nil
 }
 
